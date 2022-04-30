0f974a92b945eccdff94a38dbafc21cb0efcc475
RainyKnight <stoltmanlucas@gmail.com>
2022-04-29 23:41:38 -0700

    Initialize files!

---
+++ main.py
@@ -0,0 +1,8 @@
+# Created by Lucas Stoltman
+# Program 4
+# April 29th, 2022
+# CSS 340 A, Dimpsey
+# Version: 1.0
+
+from robot import Robot
+
---
+++ robot.py
@@ -0,0 +1,14 @@
+# Created by Lucas Stoltman
+# Program 4
+# April 29th, 2022
+# CSS 340 A, Dimpsey
+# Version: 1.0
+
+class Robot:
+    x = 0
+    y = 0
+
+    # constructor
+    def __init__(self, x1: int = 0, y1: int = 0):
+        x = x1
+        y = y1
