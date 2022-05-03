dd6074e290016793d101a9ff101dc8dee51d8f84
RainyKnight <stoltmanlucas@gmail.com>
2022-05-02 18:33:20 -0700

    Basic outline of classes

--- main.py
+++ main.py
@@ -4,5 +4,14 @@
 # CSS 340 A, Dimpsey
 # Version: 1.0
 
+import sys
+
 from robot import Robot
+
+
+arg1 = int(sys.argv[1])
+arg2 = int(sys.argv[2])
+arg3 = int(sys.argv[3])
+
+
 
--- robot.py
+++ robot.py
@@ -3,12 +3,41 @@
 # April 29th, 2022
 # CSS 340 A, Dimpsey
 # Version: 1.0
+import sys
+
 
 class Robot:
-    x = 0
-    y = 0
+    rx = 0
+    ry = 0
+    tx = 0
+    ty = 0
+    _num_paths = 0
 
     # constructor
-    def __init__(self, x1: int = 0, y1: int = 0):
-        x = x1
-        y = y1
+    def __init__(self, x1: int = 0, y1: int = 0, x2: int = 0, y2: int = 0):
+        rx = x1
+        ry = y1
+        tx = x2
+        ty = y2
+
+    # base case
+    # check for cardinal treasure = the quickest path
+    # if x = a or y = b
+
+    # decide where treasure is
+    # if x > a, y > b
+
+
+class Point:
+    _x = 0
+    _y = 0
+
+    def __init__(self, x1, y1):
+        _x = x1
+        _y = y1
+
+    def get_x(self):
+        return _x
+
+    def get_y(self):
+        return _y
