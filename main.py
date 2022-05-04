da95703a19e0aa26930455efab5746430fb5318d
RainyKnight <stoltmanlucas@gmail.com>
2022-05-03 23:54:27 -0700

    Add TODOs for robot skeleton

--- main.py
+++ main.py
@@ -7,11 +7,17 @@
 import sys
 
 from robot import Robot
+from robot import treasure
 
 
 arg1 = int(sys.argv[1])
 arg2 = int(sys.argv[2])
 arg3 = int(sys.argv[3])
+arg4 = int(sys.argv[4])
 
 
+# make two point objects
 
+# robot | robot class
+
+# treasure | point class
--- robot.py
+++ robot.py
@@ -7,10 +7,16 @@ import sys
 
 
 class Robot:
+    # robot x
     rx = 0
+    # robot y
     ry = 0
+
+    # treasure x
     tx = 0
+    # treasure y
     ty = 0
+
     _num_paths = 0
 
     # constructor
@@ -21,23 +27,35 @@ class Robot:
         ty = y2
 
     # base case
-    # check for cardinal treasure = the quickest path
-    # if x = a or y = b
+    # TODO check for cardinal treasure = the quickest path
+    # def check_cardinal(self):
+    #     if rx = tx or ry = ty:
 
-    # decide where treasure is
+    # TODO decide where treasure is
     # if x > a, y > b
+
+    # TODO finish print
+    def __str__(self):
+        return f"{rx}, {ry}"
+
+    def __repr__(self):
+        return f"{rx}, {ry}, {tx}, {ty}"
 
 
 class Point:
     _x = 0
     _y = 0
 
-    def __init__(self, x1, y1):
-        _x = x1
-        _y = y1
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
 
     def get_x(self):
-        return _x
+        return self._x
 
     def get_y(self):
-        return _y
+        return self._y
+
+    # TODO __str__
+    # TODO __repr__
+    # TODO __eq__
