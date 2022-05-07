7a8320ba8694f0a6e77689582cf4f17e0bf094e4
RainyKnight <stoltmanlucas@gmail.com>
2022-05-07 00:06:51 -0700

    Add direction decision

--- main.py
+++ main.py
@@ -7,13 +7,23 @@
 import sys
 
 from robot import Robot
-from robot import treasure
+from robot import Point
 
 
-arg1 = int(sys.argv[1])
-arg2 = int(sys.argv[2])
-arg3 = int(sys.argv[3])
-arg4 = int(sys.argv[4])
+x1 = int(sys.argv[1])
+y1 = int(sys.argv[2])
+x2 = int(sys.argv[3])
+y2 = int(sys.argv[4])
+
+# debugging arguments
+print(f"Robot: ({x1},{y1})\n"
+      f"Treasure: ({x2},{y2})")
+
+treasure = Point(x2, y2)
+rob = Robot(x1, y1)
+
+rob.find_direction(treasure)
+print("Direction: ", rob.get_direction())
 
 
 # make two point objects
--- robot.py
+++ robot.py
@@ -6,42 +6,6 @@
 import sys
 
 
-class Robot:
-    # robot x
-    rx = 0
-    # robot y
-    ry = 0
-
-    # treasure x
-    tx = 0
-    # treasure y
-    ty = 0
-
-    _num_paths = 0
-
-    # constructor
-    def __init__(self, x1: int = 0, y1: int = 0, x2: int = 0, y2: int = 0):
-        rx = x1
-        ry = y1
-        tx = x2
-        ty = y2
-
-    # base case
-    # TODO check for cardinal treasure = the quickest path
-    # def check_cardinal(self):
-    #     if rx = tx or ry = ty:
-
-    # TODO decide where treasure is
-    # if x > a, y > b
-
-    # TODO finish print
-    def __str__(self):
-        return f"{rx}, {ry}"
-
-    def __repr__(self):
-        return f"{rx}, {ry}, {tx}, {ty}"
-
-
 class Point:
     _x = 0
     _y = 0
@@ -59,3 +23,51 @@ class Point:
     # TODO __str__
     # TODO __repr__
     # TODO __eq__
+
+
+class Robot:
+    # robot x
+    _rx = 0
+    # robot y
+    _ry = 0
+
+    # cardinal direction treasure is from robot
+    _direction = ""
+    _num_paths = 0
+
+    # constructor
+    def __init__(self, x1: int = 0, y1: int = 0):
+        self._rx = x1
+        self._ry = y1
+
+    # base case
+    # TODO check for cardinal treasure = the quickest path
+    # def check_cardinal(self):
+    #     if rx = tx or ry = ty:
+
+    def get_direction(self):
+        return self._direction
+
+    # TODO decide where treasure is
+    def find_direction(self, point: Point):
+        # same x = N/S
+        if self._rx == point.get_x():
+            self._direction = "north/south"
+        # same y = E/W
+        elif self._ry == point.get_y():
+            self._direction = "east/west"
+        elif self._rx < point.get_x() and self._ry < point.get_y():
+            self._direction = "northeast"
+        elif self._rx < point.get_x() and self._ry > point.get_y():
+            self._direction = "southeast"
+        elif self._rx > point.get_x() and self._ry < point.get_y():
+            self._direction = "northwest"
+        elif self._rx > point.get_x() and self._ry > point.get_y():
+            self._direction = "southwest"
+
+    # TODO finish print
+    def __str__(self):
+        return f"{self._rx}, {self._ry}"
+
+    def __repr__(self):
+        return f"{self._rx}, {self._ry}, {self._tx}, {self._ty}"
