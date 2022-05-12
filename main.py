6c705df130935b0474f9add46a24e99da5d6d967
RainyKnight <stoltmanlucas@gmail.com>
2022-05-11 23:35:04 -0700

    Error handling

--- main.py
+++ main.py
@@ -14,11 +14,6 @@ y1 = int(sys.argv[2])
 x2 = int(sys.argv[3])
 y2 = int(sys.argv[4])
 
-# x1 = 0
-# y1 = 0
-# x2 = 2
-# y2 = 2
-
 # debugging arguments
 # print(f"Robot: ({x1},{y1})\n"
 #       f"Treasure: ({x2},{y2})")
--- testing.py
+++ testing.py
@@ -1,14 +1,8 @@
-# Created by Lucas Stoltman
-# Program 4
-# April 29th, 2022
-# CSS 340 A, Dimpsey
-# Version: 1.0
 
 import sys
 
 from robot import Robot
 from robot import Point
-# TODO remove random
 import random
 
 # final input
